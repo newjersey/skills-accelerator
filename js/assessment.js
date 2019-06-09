346b0bde5201d0969c5b8e1d935e9700262aa7f1
Anirudh Dinesh <dudurudh@gmail.com>
2019-06-09 13:57:32 -0400

    assess-fix

--- js/assessment.js
+++ js/assessment.js
@@ -14,35 +14,36 @@ function getRadioVal(form, name) {
 
 
 var answer_correct = document.querySelectorAll('.answer-correct');
-var answer_wrong = document.querySelectorAll('.answer-wrong');
-var questions = document.querySelectorAll('.questions-form');
-var submit_index = document.querySelectorAll('.getVal');
-var buttonsCount = submit_index.length;
-var index = 0;
-var items=document.getElementsByName('questions-all-correct');
-var selectedItems=0;
-var buttonsCount = buttons.length;
-
-for (var i = 0; i <= buttonsCount; i += 1) {
-  buttons[i].onclick = function() {
-    index = parseInt(this.id)-1;
-  
-  if(getRadioVal(this.form, 'questions') == "correct")
-  {
-  
-  answer_wrong[index].classList.replace('show','hide');
-  answer_correct[index].classList.replace('hide','show');
-  
-  }
-
-  else{
-  
-  answer_correct[index].classList.replace('show','hide');
-  answer_wrong[index].classList.replace('hide','show');
-  
-  }
-  };
-}
+	var answer_wrong = document.querySelectorAll('.answer-wrong');
+	var questions = document.querySelectorAll('.questions-form');
+	var submit_index = document.querySelectorAll('.getVal');
+	var buttonsCount = submit_index.length;
+	var index = 0;
+	var items=document.getElementsByName('questions-all-correct');
+	var selectedItems=0;
+	var buttons = document.getElementsByName("getVal");
+	var buttonsCount = buttons.length;
+	
+	for (var i = 0; i <= buttonsCount; i += 1) {
+	  buttons[i].onclick = function() {
+	    index = parseInt(this.id)-1;
+	
+	  if(getRadioVal(this.form, 'questions') == "correct")
+	  {
+	
+	  answer_wrong[index].classList.replace('show','hide');
+	  answer_correct[index].classList.replace('hide','show');
+	
+	  }
+	
+	  else{
+	
+	  answer_correct[index].classList.replace('show','hide');
+	  answer_wrong[index].classList.replace('hide','show');
+	
+	  }
+	  };
+	}
 function printChecked(id, req_corr){
  
   var flag = 0;
